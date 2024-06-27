f3c34aabffc72a94062936d97a2ba4d9593546f7
franklynekirwa2 <sangkirwa2@gmail.com>
2024-06-27 15:46:13 +0300

    funcrion labs

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+ function distanceFromHqInBlocks(pickup_location){
+   const headquarters=42;
+  const distance=Math.abs(pickup_location - headquarters);
+    return distance;
+ }
+
+ function distanceFromHqInFeet(pickup_location){
+    const block_feet = 264;
+    const blocksaway = distanceFromHqInBlocks(pickup_location);
+    const feetaway = blocksaway * block_feet;
+    return feetaway;
+}
+function distanceTravelledInFeet(start_block, end_block){
+   let blocks_travelled = Math.abs(end_block - start_block);
+    let feet_travelled = blocks_travelled * 264;
+    return feet_travelled;
+}
+
+function calculatesFarePrice(start_block, end_block){
+    const feet_travelled = distanceTravelledInFeet(start_block, end_block);
+
+    if (feet_travelled <= 400) {
+        return 0;
+    } else if (feet_travelled > 400 && feet_travelled <= 2000) {
+        const fare = (feet_travelled - 400) * 0.02;
+        return fare;
+    } else if (feet_travelled > 2000 && feet_travelled <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
+
+
